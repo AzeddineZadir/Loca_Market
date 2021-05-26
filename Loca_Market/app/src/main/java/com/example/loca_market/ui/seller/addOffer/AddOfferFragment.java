168c7a9ea606e4374f662fd2344c5bffe4b0428d
Kaci Ahmed <kaciahmed3@gmail.com>
2021-05-26 18:43:42 +0200

    implementation of search product on add Offers

--- Loca_Market/app/src/main/java/com/example/loca_market/ui/seller/addOffer/AddOfferFragment.java
+++ Loca_Market/app/src/main/java/com/example/loca_market/ui/seller/addOffer/AddOfferFragment.java
@@ -1,38 +1,49 @@
 package com.example.loca_market.ui.seller.addOffer;
 
-import android.icu.util.Calendar;
-import android.icu.util.TimeZone;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.util.Pair;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.loca_market.R;
-import com.example.loca_market.ui.seller.addProduct.AddProductViewModel;
+import com.example.loca_market.data.models.Product;
+import com.example.loca_market.ui.seller.adapters.SellerAddOfferSearchProductAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class AddOfferFragment extends Fragment {
     private TextInputLayout tif_offer_titel,tif_percentage,tif_offer_product,tif_offer_begin_date,tif_offer_end_date;
     private Button b_offer_begin_date,b_add_offer;
+    private FirebaseFirestore firebaseFirestore;
+    private List<Product> productsListSearch;
+    private List<Product>allProducts;
+    private RecyclerView productSearchRecyclerView;
+    private SellerAddOfferSearchProductAdapter productSearchAdapter;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -85,13 +96,67 @@ public class AddOfferFragment extends Fragment {
             }
         });
 
+        // get all products
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        allProducts = new ArrayList<>();
+        firebaseFirestore.collection("products").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful() && task.getResult()!=null){
+                    for(DocumentSnapshot doc:task.getResult().getDocuments()){
+                        Product product=doc.toObject(Product.class);
+                        allProducts.add(product);
 
 
+                    }
+                }
+            }
+        } );
+        // search products
+        tif_offer_product= view.findViewById(R.id.tif_offer_product);
+        productsListSearch =new ArrayList<>();
+        productSearchRecyclerView = view.findViewById(R.id.offer_product_search_recycler);
+        productSearchRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false));
+        productSearchAdapter = new SellerAddOfferSearchProductAdapter(getContext(), productsListSearch);
+        productSearchRecyclerView.setAdapter(productSearchAdapter);
 
+        tif_offer_product.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().isEmpty()){
+                    productsListSearch.clear();
+                    productSearchAdapter.notifyDataSetChanged();
+                }else {
+                    productsListSearch.clear();
+                    searchProduct(s.toString());
+                    productSearchAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return view;
+    }
+
+    private void searchProduct(String text) {
+        productsListSearch.clear();
+        if(!text.isEmpty()){
+            for(Product product : allProducts){
+                if(product.getName().toLowerCase().contains(text.toLowerCase())){
+                    productsListSearch.add(product);
+                    productSearchAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+
     }
 
 }
