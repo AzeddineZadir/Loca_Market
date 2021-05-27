b0bd070c4b898d00c4fe3187880c55891d858fba
silvermoon06 <silvermoon06000@gmail.com>
2021-05-27 11:56:50 +0200

    fix interface bugs

--- Loca_Market/app/src/main/java/com/example/loca_market/ui/client/adapter/BestSellsAdapter.java
+++ Loca_Market/app/src/main/java/com/example/loca_market/ui/client/adapter/BestSellsAdapter.java
@@ -18,9 +18,9 @@ import com.example.loca_market.ui.client.Activities.ProductDetailsActivity;
 
 import java.util.List;
 
-public class BestSellsAdapter extends RecyclerView.Adapter<BestSellsAdapter.ViewHolder>{
-    private Context context;
-    private List<Product> mBestSellsList;
+public class BestSellsAdapter extends RecyclerView.Adapter<BestSellsAdapter.ViewHolder> {
+    private final Context context;
+    private final List<Product> mBestSellsList;
 
     public BestSellsAdapter(Context context, List<Product> mBestSellsList) {
         this.context = context;
@@ -30,40 +30,55 @@ public class BestSellsAdapter extends RecyclerView.Adapter<BestSellsAdapter.View
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.client_home_single_product_item,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.client_home_single_product_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            holder.mBestSellsName.setText(mBestSellsList.get(position).getName().trim());
-            holder.mBestSellsPrice.setText(mBestSellsList.get(position).getPrice()+"€");
-            Glide.with(context).load(mBestSellsList.get(position).getImageUrl()).into(holder.mBestSellsImg);
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(context, ProductDetailsActivity.class);
-                    intent.putExtra("productDetail",mBestSellsList.get(position));
-                    context.startActivity(intent);
-                }
-            });
+        Product currentProduct = mBestSellsList.get(position);
+        holder.mBestSellsName.setText(mBestSellsList.get(position).getName().trim());
+
+
+        Float newPrice = currentProduct.getPrice() - (currentProduct.getPrice() * currentProduct.getPercentage() / 100);
+        holder.mBestSellsPrice.setText(newPrice + " €");
+        if (currentProduct.getPercentage() != 0) {
+            holder.tv_product_offer_precentage.setText("- " + currentProduct.getPercentage() + " %");
+        } else {
+            holder.tv_product_offer_precentage.setVisibility(View.INVISIBLE);
+        }
+
+
+        Glide.with(context).load(mBestSellsList.get(position).getImageUrl()).into(holder.mBestSellsImg);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ProductDetailsActivity.class);
+                intent.putExtra("productDetail", mBestSellsList.get(position));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-       return mBestSellsList.size();
+        return mBestSellsList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView mBestSellsImg;
-        private TextView mBestSellsPrice;
-        private  TextView mBestSellsName;
+        private final ImageView mBestSellsImg;
+        private final TextView mBestSellsPrice;
+        private final TextView tv_product_offer_precentage;
+        private final TextView mBestSellsName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mBestSellsImg = itemView.findViewById(R.id.i_ProductImg);
-            mBestSellsPrice=itemView.findViewById(R.id.t_ProductPrice);
-            mBestSellsName=itemView.findViewById(R.id.t_ProductName);
+            mBestSellsPrice = itemView.findViewById(R.id.t_ProductPrice);
+            tv_product_offer_precentage = itemView.findViewById(R.id.tv_product_offer_precentage);
+
+
+            mBestSellsName = itemView.findViewById(R.id.t_ProductName);
 
         }
     }
